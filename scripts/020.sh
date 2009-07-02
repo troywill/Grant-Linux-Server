c6018cab532b224fc20c174b5181af9cf0ca3430
Troy Will <troydwill@gmail.com>
2009-07-03 01:50:33 +0400

    rename

--- scripts/020.sh
+++
@@ -1,6 +0,0 @@
-sudo aptitude install git-core
-wget --no-clobber http://git.savannah.gnu.org/cgit/stow.git/snapshot/stow-master.tar.gz
-
-git-stow () {
-    
-}
--- scripts/030-git-stow.sh
+++ scripts/030-git-stow.sh
@@ -1,4 +1,6 @@
 git clone troywill@git.sv.gnu.org:/srv/git/stow.git
+wget --no-clobber http://git.savannah.gnu.org/cgit/stow.git/snapshot/stow-master.tar.gz
+
 
 
 
