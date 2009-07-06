999d2cbf44bab671134d13836fbd27e29f812f21
troywill <troydwill@gmail.com>
2009-07-06 12:04:57 -0700

    Minor refactoring

--- scripts/010-post-install.sh
+++ scripts/010-post-install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # TDW
 source ./common-lib.sh
-apt-get update
-apt-get install aptitude
-aptitude safe-upgrade
-aptitude install emacs22-nox
-aptitude install sudo
-adduser troy
+apt-get update && apt-get install aptitude && aptitude safe-upgrade
+aptitude install emacs22-nox && aptitude install sudo
+
