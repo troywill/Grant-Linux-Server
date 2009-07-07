beb32912aacc1509858a9fedd8e95a1d85b8c977
Troy <troy@www.troywill.com>
2009-07-07 07:30:15 +0400

    minor

--- scripts/060-install-ruby.sh
+++ scripts/060-install-ruby.sh
@@ -1,7 +1,5 @@
 # 2009-07-04
 INSTALL='sudo aptitude install'
 wget --no-clobber ftp://ftp.ruby-lang.org/pub/ruby/ruby-1.8.7-p174.tar.bz2
-${INSTALL} ncurses-dev
-${INSTALL} libssl-dev
-${INSTALL} zlib1g-dev
-${INSTALL} libreadline5-dev
+${INSTALL} ncurses-dev libssl-dev zlib1g-dev libreadline5-dev
+
