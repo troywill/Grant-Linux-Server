ef8b473e42202e79638934b569756e963dd8cfdd
Troy Will <troy@www.troywill.info>
2009-07-09 23:09:06 +0400

    minor update

--- RapidVPS.com/Debian5.0/scripts/070-install-rails.sh
+++ RapidVPS.com/Debian5.0/scripts/070-install-rails.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 I='sudo gem install --verbose --no-ri --no-rdoc'
-${I} rails mongrel capistrano sqlite3-ruby mysql
+#${I} rails mongrel capistrano sqlite3-ruby mysql
+${I} rails mongrel capistrano sqlite3-ruby passenger
+
 
