50308ec0c5d8c444d5f3b0b3459c4c98827754ff
Troy Will <troy@www.troywill.info>
2009-07-09 21:35:54 +0400

    deleted:    reference/Rsync/example1.sh deleted:    reference/Rsync/rsync.sh deleted:    reference/Rsync/rsyncd.conf deleted:    reference/bash_aliases deleted:    reference/stowrc deleted:    reference/sudoers

--- reference/Rsync/rsync.sh
+++
@@ -1,4 +0,0 @@
-# rsync --archive --verbose --delete troywill.com::etc etc
-# rsync --archive --verbose --delete troywill.com::var var
-rsync --archive --verbose --delete troywill.com::tdw stow
-rsync --archive --verbose --delete troywill.com::home home
