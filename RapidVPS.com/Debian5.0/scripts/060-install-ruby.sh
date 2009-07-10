5518ec57d4aca5d10720e6256eca28a8b76c201c
troy <troy@www.troywill.info>
2009-07-10 21:38:21 +0400

    update

--- RapidVPS.com/Debian5.0/scripts/060-install-ruby.sh
+++ RapidVPS.com/Debian5.0/scripts/060-install-ruby.sh
@@ -1,5 +1,4 @@
 # 2009-07-04
 INSTALL='sudo aptitude install'
-wget --no-clobber ftp://ftp.ruby-lang.org/pub/ruby/ruby-1.8.7-p174.tar.bz2
+wget --no-clobber ftp://ftp.ruby-lang.org/pub/ruby/ruby-1.8.7-p174.tar.bz2 &
 ${INSTALL} ncurses-dev libssl-dev zlib1g-dev libreadline5-dev
-
